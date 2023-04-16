e4187a2d2e6ecb4d7b99b40d5f96af7904dedd56
Vlera Islami <117523183+VleraIslami@users.noreply.github.com>
2023-04-16 03:02:35 +0200

    Added a loop to get the input or to quit the loop

--- CodePart.py
+++ CodePart.py
@@ -3,3 +3,9 @@ import base64
 def encode_decode_base64():
     while True:
         user_input = input("Enter a string to encode/decode with base64 (q to quit): ")
+if user_input.lower() == 'q':
+            break
+
+        # Encoding the input string using base64
+        encoded_bytes = base64.b64encode(user_input.encode('utf-8'))
+        encoded_string = encoded_bytes.decode('utf-8')
